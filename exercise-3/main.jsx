fefaa24336eecd3eb357f0c176d25bf6f55c1864
iGuest <eunmyun>
2016-11-01 14:09:38 -0700

    in class

--- exercise-3/main.jsx
+++ exercise-3/main.jsx
@@ -11,6 +11,13 @@ var employeeData = [
 // Create an EmployeeRow element to render a row of information for an employee
 var EmployeeRow = React.createClass({
     // Define rendering function
+    render:function() {
+        return(<tr className={this.props.title}>
+                <td>{this.props.name}</td>
+                <td>{this.props.title}</td>
+                <td>{this.props.salary}</td>
+                </tr>)
+    }
 });
 
 // Create an EmployeeTable element in which to render your EmployeeRows
@@ -27,7 +34,8 @@ var EmployeeTable = React.createClass({
                         </tr>
 
 
-                        {// Enter employee rows here!!!!
+                        {this.props.data.map(function(d, i))
+                        // Enter employee rows here!!!!
                         }
 
 
